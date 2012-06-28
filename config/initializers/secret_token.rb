8b38c2d3455e879990a8f6488aaad6f73c713af4
Scott <scott.bold@gmail.com>
2012-06-28 18:16:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventhero::Application.config.secret_token = '93162533030a97a688e6f749f6e4d1a508de2a114006eea4b44d5b7044efd5f9a2f04cc87dcda878226626384bf445504a82154b70afb7d6e63a991c8581800d'
